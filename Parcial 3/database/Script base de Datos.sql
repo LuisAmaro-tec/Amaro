7c2ab4eaff9bedcd5becf7866cf5f4d8972d3c97
LuisAmaro-Tec <l18100148@nlaredo.tecnm.mx>
2021-11-19 00:37:49 -0600

    Script base de datos

--- Parcial 3/database/Script base de Datos.sql
+++ Parcial 3/database/Script base de Datos.sql
@@ -1,8 +1,18 @@
-/*DROP TABLE IF EXISTS L18100148;*/
+CREATE DATABASE WebL18100148;
 
-CREATE DATABASE L18100148;
+USE WebL18100148;
 
-USE L18100148;
+CREATE TABLE usuarios
+(
+id_cred_usuarios varchar(50) not null,
+usuario varchar(50) not null,
+contraseña varchar(50),
+primary key (id_cred_usuarios)
+);
+
+INSERT INTO USUARIOS (id_cred_usuarios, usuario, contraseña) VALUES ('U1','Luis','L18100148');
+INSERT INTO USUARIOS (id_cred_usuarios, usuario, contraseña) VALUES ('U2','Angel','123456');
+INSERT INTO USUARIOS (id_cred_usuarios, usuario, contraseña) VALUES ('U3','Eduardo','654321');
 
 CREATE TABLE formulario_artista
 (
@@ -16,7 +26,9 @@ nombre_album varchar(50) not null,
 num_canciones smallint not null,
 duracion_album float,
 categoria_musica varchar(50) not null,
-constraint PK_ARTISTA primary key (id_cantante) 
+id_cred_usuarios varchar(50),
+primary key (id_cantante),
+foreign key (id_cred_usuarios) references usuarios(id_cred_usuarios)
 ); 
 
 INSERT INTO FORMULARIO_ARTISTA (id_cantante, nombre_cantante, apellido_cantante, nombre_discografia, direccion_disco, telefono_disco, nombre_album, num_canciones, duracion_album, categoria_musica) VALUES ('C12','Grupo Firme','Caz','Manzanita Roja','Guerrero 2152','857496567','Desde la cierra',10,250,'Banda');
@@ -25,14 +37,4 @@ INSERT INTO FORMULARIO_ARTISTA (id_cantante, nombre_cantante, apellido_cantante,
 INSERT INTO FORMULARIO_ARTISTA (id_cantante, nombre_cantante, apellido_cantante, nombre_discografia, direccion_disco, telefono_disco, nombre_album, num_canciones, duracion_album, categoria_musica) VALUES ('C19','Angel','Quezada','Rap Trap Records','Los Toboganes 487','867458242','Infiel',8,2170,'Rap');
 INSERT INTO FORMULARIO_ARTISTA (id_cantante, nombre_cantante, apellido_cantante, nombre_discografia, direccion_disco, telefono_disco, nombre_album, num_canciones, duracion_album, categoria_musica) VALUES ('C25','Luis Angel','El Flaco','Rancho Humilde','Mexico 65','346857298','Mi ultimo deseo',10,250,'Grupero');
 
-CREATE TABLE usuarios
-(
-id_cred_usuarios varchar(50) not null,
-usuario varchar(50) not null,
-contraseña varchar(50),
-constraint PK_USUARIOS primary key (id_cred_usuarios)
-);
 
-INSERT INTO USUARIOS (id_cred_usuarios, usuario, contraseña) VALUES ('U1','Luis','L18100148');
-INSERT INTO USUARIOS (id_cred_usuarios, usuario, contraseña) VALUES ('U2','Angel','123456');
-INSERT INTO USUARIOS (id_cred_usuarios, usuario, contraseña) VALUES ('U3','Eduardo','654321');
